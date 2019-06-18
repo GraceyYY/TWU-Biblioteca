5bebc14cba1828fe56849d508c82240a70e33088
Grace <lunay0325@gmail.com>
2019-06-18 22:45:03 +0800

    Add fields to store author and publication year in Book class

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -2,8 +2,16 @@ package com.twu.biblioteca;
 
 public class Book {
     private String name;
+    private String author;
+    private int publicationYear;
 
-    public Book(String name) {
+    public Book(String name, String author, int publicationYear) {
         this.name = name;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String displayBookInfo() {
+        return this.name + " (author: " + this.author + ", publication year: " + this.publicationYear + ")";
     }
 }
