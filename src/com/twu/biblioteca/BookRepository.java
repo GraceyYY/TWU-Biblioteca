2589ef0fbe4ce94fe4602da52cd3926d87890cbc
Grace <lunay0325@gmail.com>
2019-06-18 22:45:57 +0800

    Implement the function to show books with their author and publication year

--- src/com/twu/biblioteca/BookRepository.java
+++ src/com/twu/biblioteca/BookRepository.java
@@ -8,14 +8,14 @@ public class BookRepository {
 
     public BookRepository() {
         this.bookList = new HashMap<String, Book>();
-        bookList.put("The Hunger Games", new Book("The Hunger Games"));
-        bookList.put("Harry Potter and the Order of the Phoenix", new Book("Harry Potter and the Order of the Phoenix"));
-        bookList.put("To Kill a MockingBird", new Book("To Kill a MockingBird"));
-        bookList.put("Pride and Prejudice", new Book("Pride and Prejudice"));
-        bookList.put("Twilight", new Book("Twilight"));
-        bookList.put("The Book Thief", new Book("The Book Thief"));
-        bookList.put("Animal Farm", new Book("Animal Farm"));
-        bookList.put("The Chronicles of Narnia", new Book("The Chronicles of Narnia"));
+        bookList.put("The Hunger Games", new Book("The Hunger Games","Suzanne Collins",2008));
+        bookList.put("Harry Potter and the Order of the Phoenix", new Book("Harry Potter and the Order of the Phoenix","J.K. Rowling", 2003));
+        bookList.put("To Kill a MockingBird", new Book("To Kill a MockingBird", "Harper Lee", 1960));
+        bookList.put("Pride and Prejudice", new Book("Pride and Prejudice","Jane Austen", 1813));
+        bookList.put("Twilight", new Book("Twilight","Stephenie Meyer", 2005));
+        bookList.put("The Book Thief", new Book("The Book Thief", "Markus Zusak", 2005));
+        bookList.put("Animal Farm", new Book("Animal Farm", "George Orwell", 1945));
+        bookList.put("The Chronicles of Narnia", new Book("The Chronicles of Narnia", "C.S. Lewis", 1956));
     }
 
     public Map<String, Book> getBookList() {
@@ -25,6 +25,12 @@ public class BookRepository {
     public void showAllBooks() {
         for (String name : this.bookList.keySet()) {
             System.out.println(name);
+        }
+    }
+
+    public void showAllBooksWithAuthorAndPublicationYear() {
+        for (Book book : this.bookList.values()) {
+            System.out.println(book.displayBookInfo());
         }
     }
 }
