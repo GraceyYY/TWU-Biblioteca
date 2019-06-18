668eb35cc8f55d79479fe3915a9d700094ade16d
Grace <lunay0325@gmail.com>
2019-06-18 22:15:14 +0800

    Complete the test for checking if all books' names are displayed

---
+++ test/com/twu/biblioteca/BookListTest.java
@@ -0,0 +1,45 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookListTest {
+    PrintStream console;
+    ByteArrayOutputStream bytes;
+    BookRepository bookRepository;
+
+    @Before
+    public void setUp() {
+        bytes = new ByteArrayOutputStream();
+        console = System.out;
+        System.setOut(new PrintStream(bytes));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(console);
+    }
+
+    @Test
+    public void shouldDisplayAllBooks() {
+        bookRepository = new BookRepository();
+        Map<String, Book> bookList = bookRepository.getBookList();
+        String allBooks = "";
+        for (String name :
+                bookList.keySet()) {
+            allBooks += (name + '\n');
+        }
+
+        bookRepository.showAllBooks();
+
+        assertEquals(allBooks, bytes.toString());
+    }
+
+}
