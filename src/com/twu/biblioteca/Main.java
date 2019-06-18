4d4472b6703e53ca5648986c38e5dad92b800efd
Grace <lunay0325@gmail.com>
2019-06-19 02:36:33 +0800

    Implement the function of selecting option 1 on the menu

--- src/com/twu/biblioteca/Main.java
+++ src/com/twu/biblioteca/Main.java
@@ -3,10 +3,15 @@ package com.twu.biblioteca;
 public class Main {
     private static Welcome welcome = new Welcome("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     private static Menu menu = new Menu("1. List of books");
-
-
+    private static BookRepository bookRepository = new BookRepository();
+    
     public static void init() {
         welcome.showMessage();
         menu.showMenu();
+    }
+
+    public static void selectOption(int input) {
+        bookRepository.createMockList();
+        bookRepository.showAllBooksWithAuthorAndPublicationYear();
     }
 }
---
+++ test/com/twu/biblioteca/MenuTest.java
@@ -0,0 +1,43 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class MenuTest {
+    PrintStream console = null;
+    ByteArrayOutputStream bytes = null;
+    Menu menu;
+
+    @Before
+    public void setUp() {
+        bytes = new ByteArrayOutputStream();
+        console = System.out;
+        System.setOut(new PrintStream(bytes));
+        menu = new Menu("1. List of books");
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(console);
+    }
+
+    @Test
+    public void shouldDisplayBookListWhenChooseFirstOption() {
+        BookRepository bookRepository = new BookRepository();
+        bookRepository.createMockList();
+        Map<String, Book> bookList = bookRepository.getBookList();
+        String allBooksWithAuthorAndPublicationYear = "";
+        for (Book book : bookList.values()) {
+            allBooksWithAuthorAndPublicationYear += ("| " + book.getName() + " | author: " + book.getAuthor() + " | publication year: " + book.getPublicationYear() + " |" + "\n");
+        }
+        Main.selectOption(1);
+        assertEquals(allBooksWithAuthorAndPublicationYear, bytes.toString());
+    }
+}
